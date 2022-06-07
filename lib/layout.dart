9e884656cf9dcc82bde42a90766b2a30585c1355
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-07 16:42:50 +0500

    Top Nav Drawer Menu responsive imgs

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:web_admin/helpers/responsiveness.dart';
 import 'package:web_admin/widgets/large_screen.dart';
 import 'package:web_admin/widgets/small_screen.dart';
+import 'package:web_admin/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  // const SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-        ),
+        key: scaffoldKey,
+        extendBodyBehindAppBar: true,
+        appBar: topNavigationBar(context, scaffoldKey),
         body: ResponsiveWidget(
           largeScreen: LargeScreen(),
           smallScreen: SmallScreen(),
---
+++ lib/widgets/top_nav.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:web_admin/helpers/responsiveness.dart';
+
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                ),
+              ],
+            )
+          : IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                key.currentState?.openDrawer();
+              }),
+      elevation: 0,
+    );
