affb43b4ce667a24c0c07a0c9ce417a27cf984cf
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-07 15:13:19 +0500

    Layout created

---
+++ lib/layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class SiteLayout extends StatelessWidget {
+  const SiteLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Row(
+        children: [
+          Expanded(
+              child: Container(
+            color: Colors.red,
+          )),
+          Expanded(flex: 5, child: Container(color: Colors.blue))
+        ],
+      ),
+    );
+  }
+}
