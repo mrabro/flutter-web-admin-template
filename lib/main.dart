5ccce72bef0aa96586e8c82decac9f4e0ffa2336
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-07 19:03:59 +0500

    part 14, no visual changes, only routing work done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:get/get.dart';
+import 'package:web_admin/controllers/menu_controller.dart';
 import 'layout.dart';
 
 void main() {
+  Get.put(MenuController);
   runApp(MyApp());
 }
 
---
+++ lib/routing/routes.dart
@@ -0,0 +1,13 @@
+const rootRoute = "/";
+
+const overviewPageRoute = "Overview";
+const driversPageRoute = "Drivers";
+const clientsPageRoute = "Clients";
+const authenticationPageRoute = "Authentication";
+
+List sideMenuItems = [
+  overviewPageRoute,
+  driversPageRoute,
+  clientsPageRoute,
+  authenticationPageRoute
+];
