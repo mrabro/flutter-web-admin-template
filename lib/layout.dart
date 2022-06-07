fda9ba2e5b8581f082e35ed11b18865ec8749eb9
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-07 18:38:38 +0500

    Top nav (added items to top nav and drawer with custom text widget

--- lib/layout.dart
+++ lib/layout.dart
@@ -14,6 +14,7 @@ class SiteLayout extends StatelessWidget {
         key: scaffoldKey,
         extendBodyBehindAppBar: true,
         appBar: topNavigationBar(context, scaffoldKey),
+        drawer: Drawer(),
         body: ResponsiveWidget(
           largeScreen: LargeScreen(),
           smallScreen: SmallScreen(),
--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:web_admin/constants/style.dart';
 import 'package:web_admin/helpers/responsiveness.dart';
+import 'package:web_admin/widgets/custom_text.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
@@ -20,5 +22,85 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
               onPressed: () {
                 key.currentState?.openDrawer();
               }),
+      title: Container(
+        child: Row(
+          children: [
+            Visibility(
+                visible: !ResponsiveWidget.isSmallScreen(context),
+                child: CustomText(
+                  text: "Dash",
+                  color: dark,
+                  size: 20,
+                  weight: FontWeight.bold,
+                )),
+            Expanded(child: Container()),
+            IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: dark,
+                ),
+                onPressed: () {}),
+            Stack(
+              children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.notifications,
+                      color: dark.withOpacity(.7),
+                    ),
+                    onPressed: () {}),
+                Positioned(
+                  top: 7,
+                  right: 7,
+                  child: Container(
+                    width: 12,
+                    height: 12,
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        color: active,
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: light, width: 2)),
+                  ),
+                )
+              ],
+            ),
+            Container(
+              width: 1,
+              height: 22,
+              color: lightGrey,
+            ),
+            SizedBox(
+              width: 24,
+            ),
+            CustomText(
+              text: "Santos Enoque",
+              color: lightGrey,
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: active.withOpacity(.5),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.all(2),
+                child: CircleAvatar(
+                  backgroundColor: light,
+                  child: Icon(
+                    Icons.person_outline,
+                    color: dark,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      iconTheme: IconThemeData(color: dark),
       elevation: 0,
+      backgroundColor: Colors.white,
     );
