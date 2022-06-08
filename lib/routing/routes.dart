f3eb9c7923d6a4af6d1e9eac572bb81f8a228b86
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-08 18:53:06 +0500

    Routes Modified

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,13 +1,27 @@
 const rootRoute = "/";
 
-const overviewPageRoute = "Overview";
-const driversPageRoute = "Drivers";
-const clientsPageRoute = "Clients";
-const authenticationPageRoute = "Authentication";
+const overviewPageDisplayName = "Overview";
+const overviewPageRoute = "/overview";
 
-List sideMenuItems = [
-  overviewPageRoute,
-  driversPageRoute,
-  clientsPageRoute,
-  authenticationPageRoute
+const driversPageDisplayName = "Drivers";
+const driversPageRoute = "/drivers";
+
+const clientsPageDisplayName = "Clients";
+const clientsPageRoute = "/clients";
+
+const authenticationPageDisplayName = "Log out";
+const authenticationPageRoute = "/auth";
+
+class MenuItem {
+  final String name;
+  final String route;
+
+  MenuItem(this.name, this.route);
+}
+
+List<MenuItem> sideMenuItemRoutes = [
+  MenuItem(overviewPageDisplayName, overviewPageRoute),
+  MenuItem(driversPageDisplayName, driversPageRoute),
+  MenuItem(clientsPageDisplayName, clientsPageRoute),
+  MenuItem(authenticationPageDisplayName, authenticationPageRoute),
 ];
