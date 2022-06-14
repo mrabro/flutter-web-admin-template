2e71d67063899480b0891d5068e0d468d17e3e1e
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-14 17:06:10 +0500

    card widgets called conditionaly to screen size

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:web_admin/constants/controllers.dart';
+import 'package:web_admin/helpers/responsiveness.dart';
+import 'package:web_admin/pages/overview/widgets/overview_cards_large.dart';
+import 'package:web_admin/pages/overview/widgets/overview_cards_medium.dart';
+import 'package:web_admin/pages/overview/widgets/overview_cards_small.dart';
 // import 'package:web_admin/helpers/responsiveness.dart';
 // import 'package:web_admin/constants/controllers.dart';
 // import 'package:web_admin/pages/overview/widgets/available_drivers_table.dart';
@@ -16,9 +22,35 @@ class OverViewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomText(text: "Overview Page"),
-    );
+    return Container(
+        child: Column(
+      children: [
+        Obx(() => Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(
+                        top: ResponsiveWidget.isSmallScreen(context) ? 56 : 6),
+                    child: CustomText(
+                        text: menuController.activeItem.value,
+                        size: 24,
+                        weight: FontWeight.bold))
+              ],
+            )),
+        Expanded(
+            child: ListView(
+          children: [
+            if (ResponsiveWidget.isLargeScreen(context) ||
+                ResponsiveWidget.isMediumScreen(context))
+              if (ResponsiveWidget.isCustomSize(context))
+                const OverviewCardsMediumScreen()
+              else
+                const OverviewCardsLargeScreen()
+            else
+              const OverviewCardsSmallScreen(),
+          ],
+        ))
+      ],
+    ));
   }
 }
 // class OverviewPage extends StatelessWidget {
