98741f1585d50778a74cce49b31f55a6d0a84166
Mohammad Rafi Abro <mrafiabro@hotmail.com>
2022-06-14 17:03:51 +0500

    responsive widget

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -7,14 +7,14 @@ const int customScreenSize = 1100;
 
 class ResponsiveWidget extends StatelessWidget {
   final Widget largeScreen;
-  final Widget mediumScreen;
-  final Widget smallScreen;
+  final Widget? mediumScreen;
+  final Widget? smallScreen;
   final Widget customScreen;
   const ResponsiveWidget(
       {Key? key,
       required this.largeScreen,
-      required this.mediumScreen,
-      required this.smallScreen,
+      this.mediumScreen,
+      this.smallScreen,
       required this.customScreen})
       : super(key: key);
 
